26e4a280446a762da44b4a8da8d4f99977451cc9
Dumi-Web <clive.dumisani@gmail.com>
2020-05-21 15:50:16 +0200

    spring boot part1

--- src/main/java/com/dumisani/spring/part1/wsdl/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://spring.io/guides/gs-producing-web-service", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.dumisani.spring.part1.wsdl;
